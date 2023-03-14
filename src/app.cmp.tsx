78193f0dfb11179f41040391682d92b1a3e3458d
Gleb Ambrazhevich <glebikx@gmail.com>
2023-03-14 16:12:50 +0100

    feat(dashboard): wrapped application with react-query provider

--- src/app.cmp.tsx
+++ src/app.cmp.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ROUTES from './config/routes';
 import { AuthProvider } from './context/auth.context';
@@ -6,33 +10,37 @@ import { NotificationProvider } from './context/notification.context';
 import ThemeProvider from './context/theme.context';
 import ProtectedRoute from './@lib/protectedRoute/protectedRoute.cmp';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <NotificationProvider>
-      <ThemeProvider>
-        <AuthProvider>
-          <Router>
-            <Routes>
-              {ROUTES.map((route) => (
-                route.protected ? (
-                  <Route
-                    key={route.path}
-                    path={route.path}
-                    element={<ProtectedRoute><route.Component /></ProtectedRoute>}
-                  />
-                ) : (
-                  <Route
-                    key={route.path}
-                    path={route.path}
-                    element={<route.Component />}
-                  />
-                )
-              ))}
-            </Routes>
-          </Router>
-        </AuthProvider>
-      </ThemeProvider>
-    </NotificationProvider>
+    <QueryClientProvider client={queryClient}>
+      <NotificationProvider>
+        <ThemeProvider>
+          <AuthProvider>
+            <Router>
+              <Routes>
+                {ROUTES.map((route) => (
+                  route.protected ? (
+                    <Route
+                      key={route.path}
+                      path={route.path}
+                      element={<ProtectedRoute><route.Component /></ProtectedRoute>}
+                    />
+                  ) : (
+                    <Route
+                      key={route.path}
+                      path={route.path}
+                      element={<route.Component />}
+                    />
+                  )
+                ))}
+              </Routes>
+            </Router>
+          </AuthProvider>
+        </ThemeProvider>
+      </NotificationProvider>
+    </QueryClientProvider>
   );
 }
 
