c975b7956e97ac02c202695a661d31b258388c84
Gleb Ambrazhevich <glebikx@gmail.com>
2023-03-17 01:36:05 +0100

    feat(game): implemented square component

---
+++ src/pages/game/components/square.cmp.tsx
@@ -0,0 +1,33 @@
+import React, { memo } from 'react';
+import { styled } from '@mui/material';
+
+interface ISquare {
+  value: number | null;
+  onClick: (idx: number) => void;
+  index: number;
+}
+
+const Square = memo(({ value, onClick, index }: ISquare) => (
+  <StyledSquare className="Square" onClick={() => onClick(index)}>
+    {value}
+  </StyledSquare>
+));
+
+const StyledSquare = styled('div')`
+  background: #fff;
+  border: 1px solid #999;
+  float: left;
+  font-size: 56px;
+  font-weight: bold;
+  width: 120px;
+  height: 120px;
+  line-height: 120px;
+  padding: 0;
+  text-align: center;
+
+  &:focus {
+    position: relative;
+  }
+`;
+
+export default Square;
